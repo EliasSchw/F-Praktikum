54783dbd9ba031a44c35ebb0e78572b3ed7f76b1
LordBlob <schwarzkopf2003.e@gmail.com>
2025-03-21 16:11:41 +0100

    Refactor reflectivity calculation and update plot for multiple tau values

--- SolidStateOptics/Theory/Theoretical Curve Reflection.py
+++ SolidStateOptics/Theory/Theoretical Curve Reflection.py
@@ -7,39 +7,46 @@ e = const.e
 m_eff = 1*const.m_e
 epsilon_0 = const.epsilon_0
 N_Dichte = 5.9*10**28
-tau = 0.1*10**-12
+tau1 = 0.1*10**-12
+tau2 = 0.01*10**-12
+tau3 = 0.001*10**-12
+tau4 = 0.0001*10**-12
 c=const.c
 d = 100 *10**-5 #dicke si undoped
 k_max = 1*10**8
 
+def calculateReflectivity(d, tau, k):
+    omega = c*k
+    omega_p = np.sqrt(N_Dichte*e**2/(m_eff*epsilon_0))
+    epsilon = 1-omega_p**2*tau**2/(1+omega**2*tau**2)+1j*omega_p**2*tau/omega/(1+omega**2*tau**2)
+    N = np.sqrt(epsilon)
 
-
-k = np.linspace(0.001,k_max,1000)
-omega = c*k
-omega_p = np.sqrt(N_Dichte*e**2/(m_eff*epsilon_0))
-epsilon = 1-omega_p**2*tau**2/(1+omega**2*tau**2)+1j*omega_p**2*tau/omega/(1+omega**2*tau**2)
-N = np.sqrt(epsilon)
-#R_Halbraum = np.abs((N - 1)/(N + 1))**2
-
-
-#Gleichung 1.60 skript
-expo = 1j*2*omega*N*d/c
-r=(np.exp(expo)-1)*(1-N)/(np.exp(expo)*(1-N)-(1+N))
-R_FabryPerot = np.abs(r)**2
+    #Gleichung 1.60 skript
+    expo = 1j*2*omega*N*d/c
+    r=(np.exp(expo)-1)*(1-N)/(np.exp(expo)*(1-N)-(1+N))
+    R_FabryPerot = np.abs(r)**2
+    return R_FabryPerot
 
 
 fig, ax = plt.subplots()
         
-ax.plot(k,R_FabryPerot)
+k = np.linspace(0, k_max, 10000)
+ax.plot(k, calculateReflectivity(d, tau1, k), linewidth=2)
+ax.plot(k, calculateReflectivity(d, tau2, k), linewidth=2)
+ax.plot(k, calculateReflectivity(d, tau3, k), linewidth=2)
+ax.plot(k, calculateReflectivity(d, tau4, k), linewidth=2)
+
+
 
 #ax.set_xticks([0, 0.25, 0.5, 0.75, 1], [r'$\Gamma$', 'X', 'M', 'Y', r'$\Gamma$'], size=14)
 
 ax.set_ylabel('Reflectivity R')
-ax.set_xlabel('wavenumber k /' + r'$m^{-1}$')
+ax.set_xlabel('wavenumber k / ' + r'$m^{-1}$')
 ax.set_title('Reflectivity of a Fabry-Perot-Interferometer')
-#ax.set_xlim(left=0, right=2)
-#ax.set_ylim(bottom=-3, top=3)
-ax.grid()
+ax.tick_params(axis='both', direction='in', which='both', top=True, right=True)
+ax.set_xlim(left=0, right=k_max)
+ax.set_ylim(bottom=0, top=1)
+#ax.grid()
 
 plt.savefig('Paper/Images/foo.png', dpi=400)
 from PIL import Image
