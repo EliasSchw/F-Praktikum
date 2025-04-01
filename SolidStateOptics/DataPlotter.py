ac963df0f42882fc3a38dbd3e139b6b96762b691
LordBlob <schwarzkopf2003.e@gmail.com>
2025-04-01 15:45:30 +0200

    Refactor DataPlotter module; update save_and_open function to use relative paths and improve file handling

--- SolidStateOptics/DataPlotter.py
+++ SolidStateOptics/DataPlotter.py
@@ -4,11 +4,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
 from DataReader import read_dpt_file
 
-plt.figure(figsize=(10, 6))
 
+plt.figure(figsize=(10, 6))
 def plot_data(data):
     """
     Plots the data using matplotlib.
@@ -21,10 +20,7 @@ def plot_data(data):
     y_values = [point[1] for point in data]
     
 
-   
     plt.plot(x_values, y_values, '-', linewidth=0.3)  # Plot as single points
-    plt.xlabel('wave number k / ' + r'$cm^{-1}$')
-    plt.ylabel('Transmission T')
     plt.grid(True)
     plt.tick_params(axis='both', direction='in', which='both', top=True, right=True)
     plt.xlim(left=min(x_values), right=max(x_values))
@@ -32,20 +28,19 @@ def plot_data(data):
 
 def save_and_open(title="foo", filename="foo"):
     plt.title(title)
-    plt.savefig(filename + '.png', dpi=600)
+    plt.savefig('Paper\\Images\\'+filename + '.png', dpi=600)
     from PIL import Image
-    Image.open(filename + ".png").show()
+    Image.open("Paper\\Images\\"+filename + ".png").show()
     plt.clf()
 
 
-filepathbeginning = r'C:/Users/schwa/OneDrive/EliasOneDrive/Uni/7. Semester/F-Praktikum/F-Praktikum/F-Praktikum/SolidStateOptics/RawData/'
+#plt.xlabel('wave number k / ' + r'$cm^{-1}$')
+#plt.ylabel('Transmission T')
 
 
-file1 = r'C:\Users\schwa\OneDrive\EliasOneDrive\Uni\7. Semester\F-Praktikum\F-Praktikum\F-Praktikum\SolidStateOptics\RawData\Reflection_ex4\refl_GaAs_doped_res4_N50_normalized.DPT'
-file3 = 'StoN_res4_N10_reference.DPT'
+filepathbeginning = './SolidStateOptics/RawData/'
+file1 = './SolidStateOptics/RawData/Reflection_ex4/refl_GaAs_doped_res4_N50_normalized.DPT'
 
-# Example usage
+
 plot_data(read_dpt_file(file1))
-#plot_data(read_dpt_file(filepathbeginning + file2))
-#plot_data(read_dpt_file(filepathbeginning + file3))
 save_and_open()
