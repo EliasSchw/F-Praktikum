a9e014d59963e1addf21c4cba1e9788951dc5b7a
Lukas <lukasxhein@gmail.com>
2025-04-04 09:28:39 +0200

    Entferne nicht verwendete Bilddatei und aktualisiere die Theorie- und Plot-Skripte zur Verbesserung der Datenvisualisierung und -analyse

--- SolidStateOptics/GasAbsorption.py
+++ SolidStateOptics/GasAbsorption.py
@@ -1,11 +1,15 @@
 from matplotlib import pyplot as plt
 from DataPlotter import plot_data, save_and_open
 from DataReader import read_dpt_file
+from scipy.signal import find_peaks
+import numpy as np
 
+# Dateipfade definieren
 filepathNormalized = './SolidStateOptics/RawData/gasAbsorption/gas_absorption_res03_N20_normalized.DPT'
 filepathSample = './SolidStateOptics/RawData/gasAbsorption/gas_absorption_res03_N20_sample.DPT'
 filepathReference = './SolidStateOptics/RawData/gasAbsorption/gas_absorption_res03_N20_reference.DPT'
 
+# Daten laden
 dataSample = read_dpt_file(filepathSample)
 dataReference = read_dpt_file(filepathReference)
 
@@ -15,8 +19,20 @@ dataReference = read_dpt_file(filepathReference)
 #    divided_data.append([s[0], s[1] / r[1]])
 #plot_data(divided_data)
 
+# Daten laden und plotten
+dataNormalized = read_dpt_file(filepathNormalized)
+wave_numbers = [point[0] for point in dataNormalized]
+transmissions = [point[1] for point in dataNormalized]
+plot_data(dataNormalized)
 
-plot_data(read_dpt_file(filepathNormalized))
+# Manuelle Positionen der Dips und y-Werte für die Nummerierung
+dip_positions = [2350, 3750, 5320, 7180]
+y_positions = [0.64, 0.7, 0.965, 1]  # Angepasste y-Werte für die Nummerierung
+
+for i, (dip, y_pos) in enumerate(zip(dip_positions, y_positions)):
+    # Zahl direkt an den gegebenen Koordinaten platzieren
+    plt.text(dip, y_pos, f'({i+1})', color='black', fontsize=10, ha='center')  # Nummerierung zentriert an den Koordinaten
+
 plt.xlabel('wave number k / ' + r'$cm^{-1}$')
 plt.ylabel('Transmission T')
 
--- SolidStateOptics/Theory/TheoreticalCurveReflection.py
+++ SolidStateOptics/Theory/TheoreticalCurveReflection.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 import os, sys
 sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]))
+from macroswriter import writeLatexMacro  # Import macroswriter module
 
 # Konstanten
 e = const.e
@@ -48,7 +49,7 @@ k_plasma = omega_plasma / c
 def plotReflectivity(d, tau1, tau2, tau3, tau4, tau5, k_max):
     fig, ax = plt.subplots()
     
-    k = np.linspace(0.1, k_max, 10000)
+    k = np.linspace(0, k_max, 10000)
 
     taus = np.logspace(np.log10(tau1), np.log10(tau5), 50)  # Generate 50 tau values logarithmically spaced
     for tau in taus:
@@ -71,9 +72,60 @@ def plotReflectivity(d, tau1, tau2, tau3, tau4, tau5, k_max):
 
     plt.savefig('Paper/Images/foo.png', dpi=400)
     from PIL import Image
-    Image.open("Paper/Images/foo.png").show()
+    #Image.open("Paper/Images/foo.png").show()
     plt.clf()
     plt.close()
 
 plotReflectivity(d, tau1, tau2, tau3, tau4, tau5, k_max)
 
+
+writeLatexMacro('N_Dichte',np.round(N_Dichte,4))
+
+#Lukas
+
+def calculateReflectivitySemiconductor(epsilon_inf, omega_LO, omega_TO, gamma, d, k, N_DichteSemicon, tau):
+    omega = c * k
+    epsilon_S = epsilon_inf * (1 + (omega_LO**2 - omega_TO**2) / (omega_TO**2 - omega**2 - 1j * omega * gamma))
+    sigma = (N_DichteSemicon * e**2 * tau) / (m_eff_Semicon) * (1 / (1 - 1j * omega * tau))
+    epsilon = epsilon_S + 1j * sigma / (omega * epsilon_0)
+    N_S = np.sqrt(epsilon)
+    expo = 1j * 2 * omega * N_S * d / c
+    r = (np.exp(expo) - 1) * (1 - N_S) / (np.exp(expo) * (1 - N_S) - (1 + N_S))
+    R_S = np.abs(r)**2  # Normierung entfernt
+    return R_S
+
+# Berechnung der Plasmafrequenz und des zugehörigen Wellenvektors für den Halbleiter-Plot
+omega_plasma_semicon = np.sqrt(N_DichteSemicon * e**2 / (m_eff_Semicon * epsilon_0))
+k_plasma_semicon = omega_plasma_semicon / c
+
+# Entfernen der Plasmafrequenzberechnung und der vertikalen Linie aus dem zweiten Plot
+k = np.linspace(1, k_max_semicon, 50000)  # Startwert auf 0.1 gesetzt, höhere Auflösung
+
+taus = [tau1, tau2, tau3, tau4]
+for tau in taus:
+    R_S = calculateReflectivitySemiconductor(epsilon_inf, omega_LO, omega_TO, gamma, d, k, N_DichteSemicon, tau)
+    # Überprüfung, ob R_S gültige Werte enthält
+    if np.all(np.isnan(R_S)) or np.all(R_S == 0):
+        print(f"Warnung: Alle Werte von R_S für τ={tau} sind ungültig oder Null.")
+    else:
+        plt.plot(k, R_S, label=f'τ={tau*1e12:.4f} ps')
+
+# Vertikale gestrichelte Linie bei k_plasma_semicon
+plt.axvline(x=k_plasma_semicon, color='black', linestyle='--', label=r'$k_{plasma}$')
+
+# Achsentitel, Legende und Speichern
+plt.xlabel('wave number k / ' + r'$m^{-1}$')
+plt.ylabel('reflectivity R')
+plt.title('Theoretical reflectivity of a semiconductor')
+plt.legend()
+plt.tick_params(axis='both', direction='in', which='both', top=True, right=True)
+plt.xlim(left=0, right=k_max_semicon)
+plt.ylim(bottom=0, top=1)
+
+plt.savefig('Paper/Images/semi.png', dpi=400)
+from PIL import Image
+Image.open("Paper/Images/semi.png").show()
+plt.clf()
+
+
+writeLatexMacro('N_Dichte',np.round(N_Dichte,4))
